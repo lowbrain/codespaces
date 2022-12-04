bfddaa506a5ed15f24a32116b7c835281357b900
lowbrain <83059975+lowbrain@users.noreply.github.com>
2022-12-04 09:44:55 +0000

    aaaa

---
+++ .devcontainer/wazi/postCreateCommand.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+echo AAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+id
+pwd
+# WAZI4VSC_URL="https://public.dhe.ibm.com/ibmdl/export/pub/software/htp/zos/tools/wazi/vscode/2.3.0/WDEU-C9SN33_Wazi_for_VS_Code_V2.3.0.zip"
+# WAZI4VSC_FILE="w4vsc.zip"
+
+# curl -o ${WAZI4VSC_FILE} ${WAZI4VSC_URL}
+# unzip ${WAZI4VSC_FILE} -o ${HOME}/Wazi_for_VS_Code_v2.3.0/
+# rm -f ${WAZI4VSC_FILE}
